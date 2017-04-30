c028adcb9f7a55db155d5203f24a6bce4e621cfb
Martin Nally <mnally@google.com>
2017-04-30 08:06:58 -0700

    add patchInternalResourceThen function

--- http-helper-functions.js
+++ http-helper-functions.js
@@ -14,6 +14,7 @@ const INTERNALURLPREFIX = ''
 const INTERNAL_SY_ROUTER_PORT = process.env.INTERNAL_SY_ROUTER_PORT
 const SHIPYARD_PRIVATE_SECRET = process.env.SHIPYARD_PRIVATE_SECRET !== undefined ? new Buffer(process.env.SHIPYARD_PRIVATE_SECRET).toString('base64') : undefined
 const MIN_TOKEN_VALIDITY_PERIOD = process.env.MIN_TOKEN_VALIDITY_PERIOD || 5000
+const CHECK_PERMISSIONS = process.env.CHECK_PERMISSIONS == 'false' ? false : true
 
 const fs = require('fs')
 
@@ -158,6 +159,17 @@ function withInternalResourceDo(res, pathRelativeURL, headers, callback) {
   })
 }
 
+function patchInternalResourceThen(res, pathRelativeURL, headers, patch, callback) {
+  sendInternalRequestThen(res, 'PATCH', pathRelativeURL, headers, patch, function(clientRes) {
+    getClientResponseObject(res, clientRes, headers.host, body => {
+      if (clientRes.statusCode == 200)
+        callback(body)
+      else
+        internalError(res, {msg: 'unable to patch internal resource', url: pathRelativeURL, statusCode: clientRes.statusCode, body: body})
+    })
+  })
+}
+
 function flowThroughHeaders(req) {
   var headers = {}
   var reqHeaders = req.headers
@@ -245,7 +257,7 @@ function getServerPostObject(req, res, callback) {
 function getServerPostBuffer(req, callback) {
   var body = []
   req.on('data', function (data) {
-    if (body.length + data.length > 1e6)
+    if (body.reduce((total, item) => total + item.length, 0) + data.length > 1e6)
       return req.connection.destroy()
     body.push(data)
   })
@@ -575,25 +587,28 @@ function uuid4() {
 // End of section of code adapted from https://github.com/broofa/node-uuid4 under MIT License
 
 function withValidClientToken(errorHandler, token, clientID, clientSecret, authURL, callback) {
-  var claims = getClaims(token)
-  if (claims != null && claims.exp < Date.now() + MIN_TOKEN_VALIDITY_PERIOD)
-    callback()
-  else {
-    var headers = {'content-type': 'application/x-www-form-urlencoded;charset=utf-8', accept: 'application/json;charset=utf-8'}
-    var body = `grant_type=client_credentials&client_id=${clientID}&client_secret=${clientSecret}`
-    sendExternalRequestThen(errorHandler, 'POST', authURL, headers, body, function(clientRes) {
-      getClientResponseBody(clientRes, function(resp_body) {
-        if (clientRes.statusCode == 200) {
-          token = JSON.parse(resp_body).access_token
-          log('withValidClientToken', `retrieved token for: ${clientID}`)
-          callback(token)
-        } else {
-          log('withValidClientToken', `unable to retrieve token. authURL: ${authURL}, headers: ${JSON.stringify(headers)}`)
-          badRequest(errorHandler, {msg: 'unable to retrieve client token', body: resp_body})
-        }
+  if (CHECK_PERMISSIONS) {
+    var claims = getClaims(token)
+    if (claims != null && claims.exp < Date.now() + MIN_TOKEN_VALIDITY_PERIOD)
+      callback()
+    else {
+      var headers = {'content-type': 'application/x-www-form-urlencoded;charset=utf-8', accept: 'application/json;charset=utf-8'}
+      var body = `grant_type=client_credentials&client_id=${clientID}&client_secret=${clientSecret}`
+      sendExternalRequestThen(errorHandler, 'POST', authURL, headers, body, function(clientRes) {
+        getClientResponseBody(clientRes, function(resp_body) {
+          if (clientRes.statusCode == 200) {
+            token = JSON.parse(resp_body).access_token
+            log('withValidClientToken', `retrieved token for: ${clientID}`)
+            callback(token)
+          } else {
+            log('withValidClientToken', `unable to retrieve token. authURL: ${authURL}, headers: ${JSON.stringify(headers)}`)
+            badRequest(errorHandler, {msg: 'unable to retrieve client token', body: resp_body})
+          }
+        })
       })
-    })
-  }
+    }
+  } else
+    callback()
 }
 
 exports.getServerPostObject = getServerPostObject
@@ -627,5 +642,6 @@ exports.sendExternalRequest = sendExternalRequest
 exports.sendExternalRequestThen = sendExternalRequestThen
 exports.flowThroughHeaders = flowThroughHeaders
 exports.withInternalResourceDo = withInternalResourceDo
+exports.patchInternalResourceThen = patchInternalResourceThen
 exports.getClientResponseObject = getClientResponseObject
 exports.withValidClientToken = withValidClientToken
